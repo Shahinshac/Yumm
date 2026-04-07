36acb1e414000d034ebbbbec6398c1b041ba30e5
Shahinshac <shahinsha.c123@gmail.com>
2026-04-08 00:20:38 +0530

    Update pubspec.yaml dependencies

--- frontend-mobile/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ frontend-mobile/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -31,6 +31,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin google_maps_flutter_android, io.flutter.plugins.googlemaps.GoogleMapsPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.googlesignin.GoogleSignInPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin google_sign_in_android, io.flutter.plugins.googlesignin.GoogleSignInPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin shared_preferences_android, io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin", e);
